b6c39b757fe3c57bfaa96da22036fec76c63c946
Ryan Brisentine <ryanbrisentine@Ryans-MacBook-Pro.local>
2019-04-12 09:26:22 -0500

    Added comic books controller

---
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComicBookGallery.Controllers 
+{
+    public class ComicBooksController : Controller
+    {
+        public ComicBooksController()
+        {
+        }
+    }
+}
